a8b9078ebf5f4f095f9e1d826bd774dae6ee647f
Pavel Korzhenko <pksmall@live.ru>
2024-06-22 13:14:53 +0300

    add employees app

---
+++ staff_hierarchy/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for staff_hierarchy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'staff_hierarchy.settings')
+
+application = get_asgi_application()
---
+++ staff_hierarchy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for staff_hierarchy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'staff_hierarchy.settings')
+
+application = get_wsgi_application()
