f120aeb894627181c85b80dca42017f449630502
Pavel Korzhenko <pksmall@live.ru>
2024-06-22 14:10:21 +0300

    add employee list

--- employees/urls.py
+++ employees/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import employee_hierarchy, load_subordinates
+from employees.views import employee_hierarchy, load_subordinates, employee_list
 
 urlpatterns = [
   path('', employee_hierarchy, name='employee_hierarchy'),
   path('load_subordinates/<int:employee_id>/', load_subordinates, name='load_subordinates'),
+  path('list/', employee_list, name='employee_list'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 from employees.models import Employee
 
@@ -12,3 +13,19 @@ def load_subordinates(request, employee_id):
   employee = Employee.objects.get(id=employee_id)
   subordinates = employee.subordinates.all()
   return render(request, 'employees/subordinates.html', {'subordinates': subordinates})
+
+
+def employee_list(request):
+  query = request.GET.get('q')
+  sort_by = request.GET.get('sort_by', 'name')
+  employees = Employee.objects.all()
+
+  if query:
+    employees = employees.filter(
+      Q(name__icontains=query) |
+      Q(position__icontains=query) |
+      Q(email__icontains=query)
+    )
+
+  employees = employees.order_by(sort_by)
+  return render(request, 'employees/employee_list.html', {'employees': employees})
